986804d39474aee41371dca6f81987bd3a025ae8
xkrat <krajnakmatto@gmail.com>
2016-03-01 09:18:40 +0100

    Script now know most of its attributes

--- jsn.php
+++ jsn.php
@@ -3,13 +3,30 @@
 
   $TESTINFILENAME = "/home/matt/Projects/ipp/json2xml/example.json";
   $TESTOUTFILENAME = "/home/matt/Projects/ipp/json2xml/example.xml";
-  $GENERATEHEADER = true;
 
-  define('XMLHEADER', '<?xml version="1.0" encoding="UTF-8" ?>'); //standard xml header
+  $GENERATEHEADER = true;
+  $WRAPROOT = false;
+  $STRING_IS_ATTRIBUTE = true;
+  $INT_IS_ATTRIBUTE = true;
+  $INDEX_ITEMS = false;
+  $ARRAY_SIZE = false;
+
+  $WRAPROOTTEXT = "root";
+  $ARRAYTEXT = "array";
+  $ITEMTEXT = "item";
+
+/* REGEXES FOR MATCHING PARAMS */
   define('INPUTRGX', '/^(--input=.+)$/');
   define('OUTPUTRGX', '/^(--output=.+)$/');
+  define('ROOTRGX', '/^(-r=.+)$/');
+  define('ARRAYRGX', '/^(--array-name=.+)$/');
+  define('ITEMRGX', '/^(--item-name=.+)$/');
+
   define('MATCHINFILENAME', '/^--input=(.+)$/');
   define('MATCHOUTFILENAME', '/^--output=(.+)$/');
+  define('MATCHROOTRGX', '/^-r=(.+)$/');
+  define('MATCHARRAYRGX', '/^--array-name=(.+)$/');
+  define('MATCHITEMRGX', '/^--item-name=(.+)$/');
 
 
   arg_check($argv,$argc);
@@ -39,7 +56,6 @@
       if ($value == "--help" && $param_count === 1) {   //help was called, exiting with 0
         help();
       }
-
       if ($value == "-n" ){
           $GLOBALS['GENERATEHEADER'] = false;
           continue;
@@ -52,6 +68,35 @@
         $GLOBALS['TESTOUTFILENAME'] = get_filename(MATCHOUTFILENAME,$value);
         continue;
       }
+      if (preg_match(ROOTRGX, $value) === 1 ) {
+        $GLOBALS['WRAPROOT'] = true;
+        $GLOBALS['WRAPROOTTEXT'] = get_filename(MATCHROOTRGX,$value);
+        continue;
+      }
+      if (preg_match(ARRAYRGX, $value) === 1 ) {  //--array-name
+        $GLOBALS['ARRAYTAGTEXT'] = get_filename(MATCHARRAYRGX,$value);
+        continue;
+      }
+      if (preg_match(ITEMRGX, $value) === 1 ) {   //--item-name
+        $GLOBALS['ITEMTEXT'] = get_filename(MATCHITEMRGX,$value);
+        continue;
+      }
+      if ($value === "-s") { //-s
+        $GLOBALS['STRING_IS_ATTRIBUTE'] = false;
+        continue;
+      }
+      if ($value === "-i") {    //-i
+        $GLOBALS['INT_IS_ATTRIBUTE'] = false;
+        continue;
+      }
+      if ($value === "-t" || $value === "--index-items" ) {    //-i
+        $GLOBALS['INDEX_ITEMS'] = true;
+        continue;
+      }
+      if ($value === "-a" || $value === "--array-size" ) {    //-i
+        $GLOBALS['ARRAY_SIZE'] = true;
+        continue;
+      }
       $parse_error = true;
     }
     if ($parse_error) {
@@ -60,7 +105,7 @@
   }
 
   /**
-  * will read content of json file
+  * Will read content of json file
   * return json content in array
   */
   function json_read(){
@@ -81,7 +126,15 @@
       $xml->startDocument('1.0', 'UTF-8');
     }
 
+    if ($GLOBALS['WRAPROOT']) {
+      $xml->startElement($GLOBALS['WRAPROOTTEXT']);
+    }
+
     writeXML($json_data,$xml);
+
+    if ($GLOBALS['WRAPROOT']) {
+      $xml->endElement();
+    }
     $xml->endDocument();
 
     print_r($xml->outputMemory(TRUE));
@@ -103,7 +156,10 @@
       }
       else if (is_array($value)) {
         $xml->startElement("$key");     //<item>
-        $xml->startElement("array");    //<key>
+        $xml->startElement($GLOBALS['ARRAYTAGTEXT']);    //<key>
+        if ($GLOBALS['ARRAY_SIZE']) {
+          $xml->writeAttribute("size",count($value));
+        }
         writeArray($value,$xml);
         $xml->endElement();             //</array>
         $xml->endElement();             //<key>
@@ -116,22 +172,23 @@
   }
 }
 
-/**
-* Iterating through array
-*/
-function writeArray($field,$xml){
-  for ($i=0; $i < count($field); $i++) {
-    $xml->startElement("item");     //<item>
-    foreach ($field[$i] as $key => $value) {
-      $xml->startElement("$key");       //<$key>
-      write_value($value,$xml);
-      $xml->endElement();             //</key>
+  /**
+  * Iterating through array
+  */
+  function writeArray($field,$xml){
+    for ($i=0; $i < count($field); $i++) {
+      $xml->startElement($GLOBALS['ITEMTEXT']);     //<item>
+      if ($GLOBALS['INDEX_ITEMS']) {
+        $xml->writeAttribute("index",$i);
+      }
+      foreach ($field[$i] as $key => $value) {
+        $xml->startElement("$key");       //<$key>
+        write_value($value,$xml);
+        $xml->endElement();             //</key>
+      }
+      $xml->endElement();             //</item>
     }
-    $xml->endElement();             //</item>
   }
-}
-
-
 
   /**
   * Correctly writes data values to xml
@@ -139,7 +196,12 @@ function writeArray($field,$xml){
   function write_value($value,$xml){       //writing values
 
     if (is_integer($value)) {
-      $xml->writeAttribute("value",$value);
+      if ($GLOBALS['INT_IS_ATTRIBUTE']) {
+        $xml->writeAttribute("value",$value);
+      }
+      else {
+        $xml->text("$value");
+      }
     }
     elseif (is_bool($value)) {
       if($value)  $xml->writeAttribute("value","true");
