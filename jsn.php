b8a4da0dfacce246d1d5134002d575e8d36e7c65
xkrat <krajnakmatto@gmail.com>
2016-03-05 18:48:53 +0100

    refactorisation

--- jsn.php
+++ jsn.php
@@ -1,41 +1,58 @@
 #!/usr/bin/php
 <?php
 
-  $TESTINFILENAME = "/home/matt/Projects/ipp/json2xml/example.json";
-  $TESTOUTFILENAME = "/home/matt/Projects/ipp/json2xml/example.xml";
+  class Options {
+    /* ERROR CHECKING FLAGS*/
+    var $generate_header = true;
+    var $wrap_root= false;
+    var $string_is_attribute = true;
+    var $int_is_attribute = true;
+    var $index_items = false;
+    var $array_size = false;
+    var $values_to_elements = false;
+    var $change_start_index = false;
 
-  $GENERATEHEADER = true;
-  $WRAPROOT = false;
-  $STRING_IS_ATTRIBUTE = true;
-  $INT_IS_ATTRIBUTE = true;
-  $INDEX_ITEMS = false;
-  $ARRAY_SIZE = false;
-  $VALUES_TO_ELEMENTS = false;      // -l
+    var $index = 0;
+    var $wrap_root_text = 'root';
+    var $array_text = 'array';
+    var $item_text = 'item';
 
-  $WRAPROOTTEXT = "root";
-  $ARRAYTEXT = "array";
-  $ITEMTEXT = "item";
+    /*FILE VARIABLEs */
+    var $in_filename = "/home/matt/Projects/ipp/json2xml/example.json";
+    var $out_filename = "/home/matt/Projects/ipp/json2xml/example.xml";
+}
 
-/* REGEXES FOR MATCHING PARAMS */
+  /* REGEXES FOR PARSING AND MATCHING PARAMS */
   define('INPUTRGX', '/^(--input=.+)$/');
   define('OUTPUTRGX', '/^(--output=.+)$/');
   define('ROOTRGX', '/^(-r=.+)$/');
   define('ARRAYRGX', '/^(--array-name=.+)$/');
   define('ITEMRGX', '/^(--item-name=.+)$/');
+  define('STARTINDEXRGX', '/^(--start=.+)$/');
 
   define('MATCHINFILENAME', '/^--input=(.+)$/');
   define('MATCHOUTFILENAME', '/^--output=(.+)$/');
   define('MATCHROOTRGX', '/^-r=(.+)$/');
   define('MATCHARRAYRGX', '/^--array-name=(.+)$/');
   define('MATCHITEMRGX', '/^--item-name=(.+)$/');
+  define('MATCHSTARTINDEXRGX', '/^--start=(.+)$/');
 
+  main($argv,$argc); //do the harlem shake
 
-  arg_check($argv,$argc);
-  echo "$TESTINFILENAME\n";
-  echo "$TESTOUTFILENAME\n";
-  $json_data = json_read();
-  print_r($json_data);
-  write_json_to_xml($json_data);
+  /**
+  * MAIN FUNCTION OF SCRIPT
+  */
+
+  function main($argv,$argc){
+    $opt = new Options();
+
+    arg_check($argv,$argc,$opt);
+    $json_data = json_read($opt);
+    print_r($json_data);                  //DELETE !!!!!!
+    write_json_to_xml($json_data,$opt);
+
+    exit(0);
+  }
 
   /**
   * Will parse and return filename from given string
@@ -48,7 +65,7 @@
   /**
   * Check args passed to script, also calling functions handling all the functonality of script
   */
-  function arg_check($argv,$argc){
+  function arg_check($argv,$argc,$opt){
     $parse_error = false;
     foreach ($argv  as $param_count => $value) {
 
@@ -58,51 +75,57 @@
         help();
       }
       if ($value == "-n" ){
-          $GLOBALS['GENERATEHEADER'] = false;
+          $opt->generate_header = false;
           continue;
       }
       if (preg_match(INPUTRGX, $value) === 1 ) {
-        $GLOBALS['TESTINFILENAME'] = get_filename(MATCHINFILENAME,$value);
+        $opt->in_filename = get_filename(MATCHINFILENAME,$value);
         continue;
       }
       if (preg_match(OUTPUTRGX, $value) === 1 ) {
-        $GLOBALS['TESTOUTFILENAME'] = get_filename(MATCHOUTFILENAME,$value);
+        $opt->out_filename = get_filename(MATCHOUTFILENAME,$value);
         continue;
       }
       if (preg_match(ROOTRGX, $value) === 1 ) {
-        $GLOBALS['WRAPROOT'] = true;
-        $GLOBALS['WRAPROOTTEXT'] = get_filename(MATCHROOTRGX,$value);
+        $opt->wrap_root = true;
+        $opt->wrap_root_text = get_filename(MATCHROOTRGX,$value); // actually returns name of root tag not filename
         continue;
       }
       if (preg_match(ARRAYRGX, $value) === 1 ) {  //--array-name
-        $GLOBALS['ARRAYTAGTEXT'] = get_filename(MATCHARRAYRGX,$value);
+        $opt->array_text = get_filename(MATCHARRAYRGX,$value);
         continue;
       }
       if (preg_match(ITEMRGX, $value) === 1 ) {   //--item-name
-        $GLOBALS['ITEMTEXT'] = get_filename(MATCHITEMRGX,$value);
+        $opt->item_text = get_filename(MATCHITEMRGX,$value);
         continue;
       }
       if ($value === "-s") { //-s
-        $GLOBALS['STRING_IS_ATTRIBUTE'] = false;
+        $opt->string_is_attribute = false;
         continue;
       }
       if ($value === "-i") {    //-i
-        $GLOBALS['INT_IS_ATTRIBUTE'] = false;
+        $opt->int_is_attribute = false;
         continue;
       }
       if ($value === "-l") {    //-l
-        $GLOBALS['VALUES_TO_ELEMENTS'] = true;
+        $opt->values_to_elements = true;
         continue;
       }
       if ($value === "-t" || $value === "--index-items" ) {    //-i
-        $GLOBALS['INDEX_ITEMS'] = true;
+        $opt->index_items = true;
         continue;
       }
       if ($value === "-a" || $value === "--array-size" ) {    //-i
-        $GLOBALS['ARRAY_SIZE'] = true;
+        $opt->array_size = true;
+        continue;
+      }
+      if (preg_match(STARTINDEXRGX, $value) === 1 ) {   //--start
+        $opt->change_start_index = true;
+        $opt->index = get_filename(MATCHSTARTINDEXRGX,$value);
         continue;
       }
       $parse_error = true;
+      break;
     }
     if ($parse_error) {
       err("Invalid parameters, try --help for more",1); // TODO: Check value again. should be 1
@@ -113,8 +136,8 @@
   * Will read content of json file
   * return json content in array
   */
-  function json_read(){
-    $raw_json = file_get_contents($GLOBALS['TESTINFILENAME']);
+  function json_read($opt){
+    $raw_json = file_get_contents($opt->in_filename);
     $json_data = json_decode($raw_json,false);
     return $json_data;
   }
@@ -122,56 +145,56 @@
   /**
   * Will open a file, and writes converted json to file
   */
-  function write_json_to_xml($json_data){
+  function write_json_to_xml($json_data,$opt){
 
     $xml = new XMLWriter();
     $xml->openMemory();
     $xml->setIndent(true);
-    if ($GLOBALS['GENERATEHEADER']) {
+    if ($opt->generate_header) {
       $xml->startDocument('1.0', 'UTF-8');
     }
 
-    if ($GLOBALS['WRAPROOT']) {
-      $xml->startElement($GLOBALS['WRAPROOTTEXT']);
+    if ($opt->wrap_root) {
+      $xml->startElement($opt->wrap_root_text);
     }
 
-    writeXML($json_data,$xml);
+    writeXML($json_data,$xml,$opt);
 
-    if ($GLOBALS['WRAPROOT']) {
+    if ($opt->wrap_root) {
       $xml->endElement();
     }
     $xml->endDocument();
 
-    print_r($xml->outputMemory(TRUE));
-  //   $out_file = fopen($GLOBALS['TESTOUTFILENAME'], "w");
-  //   fwrite($out_file,$xml->outputMemory(TRUE));
-  //   fclose($out_file);
+    //print_r($xml->outputMemory(TRUE));
+    $out_file = fopen($opt->out_filename, "w");
+    fwrite($out_file,$xml->outputMemory(TRUE));
+    fclose($out_file);
     }
 
   /**
   * Recursively writes arrays, object, in the end data
   */
-  function writeXML($json_data,$xml){
+  function writeXML($json_data,$xml,$opt){
 
     foreach ($json_data as $key => $value) {
       if (is_object($value)) {
         $xml->startElement($key);     //<key>
-        writeXML($value,$xml);
+        writeXML($value,$xml,$opt);
         $xml->endElement();             //</key>
       }
       else if (is_array($value)) {
-        $xml->startElement("$key");     //<item>
-        $xml->startElement($GLOBALS['ARRAYTAGTEXT']);    //<key>
-        if ($GLOBALS['ARRAY_SIZE']) {
+        $xml->startElement($key);     //<item>
+        $xml->startElement($opt->array_text);    //<key>
+        if ($opt->array_size) {
           $xml->writeAttribute("size",count($value));
         }
-        writeArray($value,$xml);
+        writeArray($value,$xml,$opt);
         $xml->endElement();             //</array>
         $xml->endElement();             //<key>
         }
       else{
         $xml->startElement($key);       //wrap with <key>
-        write_value($value,$xml);
+        write_value($value,$xml,$opt);
         $xml->endElement();             //<key>
     }
   }
@@ -180,15 +203,18 @@
   /**
   * Iterating through array
   */
-  function writeArray($field,$xml){
-    for ($i=0; $i < count($field); $i++) {
-      $xml->startElement($GLOBALS['ITEMTEXT']);     //<item>
-      if ($GLOBALS['INDEX_ITEMS']) {
-        $xml->writeAttribute("index",$i);
+  function writeArray($field,$xml,$opt){
+    for ($i = 0; $i < count($field); $i++) {
+
+      check_start_index($opt);
+      $xml->startElement($opt->item_text);     //<item>
+      if ($opt->index_items) {
+        $xml->writeAttribute("index",$i+$opt->index);
       }
       foreach ($field[$i] as $key => $value) {
+
         $xml->startElement("$key");       //<$key>
-        write_value($value,$xml);
+        write_value($value,$xml,$opt);
         $xml->endElement();             //</key>
       }
       $xml->endElement();             //</item>
@@ -198,18 +224,16 @@
   /**
   * Correctly writes data values to xml
   */
-  function write_value($value,$xml){       //writing values
+  function write_value($value,$xml,$opt){       //writing values
 
-    if (is_integer($value)) {
-      if ($GLOBALS['INT_IS_ATTRIBUTE']) {
+    if (is_integer($value) ) {
+      if ($opt->int_is_attribute) {
         $xml->writeAttribute("value",$value);
       }
-      else {
-        $xml->text("$value");
-      }
+      else $xml->text("$value");
     }
     elseif (is_bool($value)) {
-      if ($GLOBALS['VALUES_TO_ELEMENTS']) {
+      if ($opt->values_to_elements) {
         if($value)  $xml->startElement("true");
         else        $xml->startElement("false");
         $xml->endElement();
@@ -219,17 +243,33 @@
         else        $xml->writeAttribute("value","false");
       }
     }
+    elseif (is_string($value)) {
+        if ( $opt->string_is_attribute ) {
+          $xml->writeAttribute("value",$value);
+        }
+        else $xml->text($value);
+    }
     elseif (empty($value)) {
       if(!is_array($value)){
-        if ($GLOBALS['VALUES_TO_ELEMENTS']) {
+        if ($opt->values_to_elements) {
           $xml->startElement("NULL");
           $xml->endElement();
         }
         $xml->writeAttribute("value","NULL");
       }
     }
-    else
-      $xml->text($value);
+    else $xml->text($value);
+  }
+
+  /**
+  * CHECK IF --start AND --index-items was entered
+  */
+  function check_start_index($opt){
+    if ($opt->change_start_index) {
+      if ($opt->index_items === false) {
+        err("ERR: use both --start and --index-items",1);
+      }
+    }
   }
 
   /**
